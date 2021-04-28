03da8790fc04fbcc1952bf12dd4aa4bfc9241fe8
Tarun <negitarun9899@gmail.com>
2021-04-28 18:41:16 +0530

    Add starting files

---
+++ main.js
@@ -0,0 +1,9 @@
+const burger = document.getElementsByClassName('burger')[0];
+const nav = document.getElementsByClassName('nav-links')[0];
+
+
+
+burger.addEventListener('click', ()=>{
+    nav.classList.toggle('nav-active');
+    burger.classList.toggle("toogle");
+})
