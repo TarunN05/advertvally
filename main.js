48912043ad7615eedb28581c0784c5e1aaca2050
Tarun <negitarun9899@gmail.com>
2021-04-30 22:38:16 +0530

    adding full website

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
-const burger = document.getElementsByClassName('burger')[0];
-const nav = document.getElementsByClassName('nav-links')[0];
+const burger = document.getElementsByClassName("burger")[0];
+const nav = document.getElementsByClassName("nav-links")[0];
 
-
-
-burger.addEventListener('click', ()=>{
-    nav.classList.toggle('nav-active');
-    burger.classList.toggle("toogle");
-})
+burger.addEventListener("click", () => {
+  nav.classList.toggle("nav-active");
+  burger.classList.toggle("toogle");
+});
