0116d949b4f4fe4421ce4b67252d14b6324d7291
TarunN05 <78839555+TarunN05@users.noreply.github.com>
2021-05-11 20:02:55 +0530

    g-sap animation

--- main.js
+++ main.js
@@ -74,6 +74,14 @@ submitBtn.addEventListener("click" ,()=>{
 });
 
 
+// G-sap animation
+const tl = gsap.timeline({defaults: {ease: 'power1.out'}});
+
+tl.to(".text", {y: "0%", duration:1, stagger: 0.25});
+tl.to(".slider", {y: "-100%" , duration: 1.5, delay:0.5});
+tl.to(".intro", {y:"-100%", duration:1}, "-=1");
+
+
 
 	
 
